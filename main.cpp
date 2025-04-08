68736a2c607736e2eda4ddec856afaeba68a5e89
Pankov Egor <87268236+Infinitized-Frog-88@users.noreply.github.com>
2025-04-08 18:02:39 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int dist[30][30];
+    for (int i = 0; i < 30; ++i) {
+        fill(dist[i], dist[i] + 26, 100000);
+    }
+    int m;
+    cin >> m;
+
+    while (m--) {
+        char u;
+        char v;
+        int w;
+        cin >> u >> v >> w;
+        dist[u - 97][v - 97] = w;
+    }
+
+    for (int k = 0; k < 26; ++k) {
+        for (int i = 0; i < 26; ++i) {
+            for (int j = 0; j < 26; ++j) {
+                if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    cout << dist['a' - 'a']['z' - 'a'] << endl;
+    return 0;
+}
